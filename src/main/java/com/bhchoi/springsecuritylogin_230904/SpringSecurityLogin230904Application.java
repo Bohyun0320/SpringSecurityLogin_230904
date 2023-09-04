cd89b90d28a083a810cc1d92899fa2c895b2d08c
choibohyun <etoile0320@naver.com>
2023-09-04 19:45:55 +0900

    23.09.04_init - spring security Login 구현 후, react 연동 예정

---
+++ src/main/java/com/bhchoi/springsecuritylogin_230904/SpringSecurityLogin230904Application.java
@@ -0,0 +1,13 @@
+package com.bhchoi.springsecuritylogin_230904;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLogin230904Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLogin230904Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhchoi/springsecuritylogin_230904/SpringSecurityLogin230904ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhchoi.springsecuritylogin_230904;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLogin230904ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
